a6b6d028957a449243bf086e06bf99b223d268ee
szqingt <szqingt@gmail.com>
2016-04-17 15:33:27 +0800

    add confirm btn

--- js/bombBox.js
+++ js/bombBox.js
@@ -2,10 +2,16 @@ define(['jquery'],function($){
 	function bombBox(){}
 
 	bombBox.prototype = {
-		alert:function(content){
-			var boudingBox = $('<div class="window_alert"></div>');
-			boudingBox.appendTo('body');
-			boudingBox.html(content);
+		alert:function(content,fn){
+			var bbox = $('<div class="bombbox_alert"></div>');
+			bbox.appendTo('body');
+			bbox.html(content);
+			var btn=$('<input class="bombbox_btn" type="button" value="确定" />');
+			btn.appendTo(bbox);
+			btn.click(function(){
+				fn && fn();
+				bbox.remove();
+			})
 		},
 		confirm:function(){},
 		prompt:function(){}
--- js/main.js
+++ js/main.js
@@ -8,7 +8,9 @@ require.config({
 require(['jquery' , 'bombBox'],function($,bombBox){
 	$('#btn').click(function(){
 				var bbox=new bombBox();
-					bbox.alert("hello world");
+					bbox.alert("hello world",function(){
+						alert("handler test")
+					});
 			}
 		)
 })
